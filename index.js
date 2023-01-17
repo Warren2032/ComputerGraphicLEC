31fd2b4fb5905cbf94529d0c11678d32f7c81e8e
Warren2032 <115253700+Warren2032@users.noreply.github.com>
2023-01-17 21:18:39 +0700

    create 10 pins
    orbit cams, and responsive

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import * as THREE from "./three.js/build/three.module.js";
+import { OrbitControls } from "./three.js/examples/jsm/controls/OrbitControls.js";
 
-let scene, camera, renderer;
+let scene, camera, renderer, control;
 
 function createLight() {
   let light = new THREE.SpotLight("#ffffff", 1);
@@ -24,13 +25,13 @@ function createFloor() {
   scene.add(floor);
 }
 
-function createPin() {
+function createPin(x,y,z) {
   //upper
   let upper = new THREE.Mesh(
     new THREE.CylinderGeometry(3, 8, 20, 12, 12),
     new THREE.MeshPhongMaterial({ color: "#ffffff" })
   );
-  upper.position.set(0, 9, 0);
+  upper.position.set(0+x, 9+y, 0+z);
   upper.castShadow = true;
 
   scene.add(upper);
@@ -40,7 +41,7 @@ function createPin() {
     new THREE.CylinderGeometry(8, 5, 12, 12, 12),
     new THREE.MeshPhongMaterial({ color: "#ffffff" })
   );
-  lower.position.set(0, -7, 0);
+  lower.position.set(0+x, -7+y, 0+z);
   lower.castShadow = true;
 
   scene.add(lower);
@@ -50,7 +51,7 @@ function createPin() {
     new THREE.CylinderGeometry(4, 3, 12, 12, 12),
     new THREE.MeshPhongMaterial({ color: "#ffffff" })
   );
-  neck.position.set(0, 23, 0);
+  neck.position.set(0+x, 23+y, 0+z);
   neck.castShadow = true;
 
   scene.add(neck);
@@ -60,7 +61,7 @@ function createPin() {
     new THREE.SphereGeometry(4, 32, 16),
     new THREE.MeshPhongMaterial({ color: "#ff0000" })
   );
-  head.position.set(0, 25, 0);
+  head.position.set(0+x, 25+y, 0+z);
   head.castShadow = true;
 
   scene.add(head);
@@ -89,20 +90,39 @@ function init() {
     0.1,
     1000
   );
-  camera.position.set(20, 60, 180);
+  // camera.position.set(20, 60, 180);
+  camera.position.set(0, 200, 0);
 
   //tidak diminta tetapi agar tampilan mirip seperti contoh
   camera.lookAt(0, 0, 0);
 
   renderer = new THREE.WebGLRenderer({ antialiasing: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
   createLight();
   createFloor();
-  createPin();
   createBall();
 
+  createPin(0,0,0);
+
+  createPin(-20,0,-5);
+  createPin(-5,0,-20);
+
+  createPin(-40,0,-10);
+  createPin(-25,0,-25);
+  createPin(-10,0,-40);
+
+  createPin(-30,0,-45);
+  createPin(-15,0,-60);
+  createPin(-45,0,-30);
+  createPin(-60,0,-15);
+  
+
+  control = new OrbitControls(camera, renderer.domElement);
   document.body.appendChild(renderer.domElement);
+
 }
 function render() {
   requestAnimationFrame(render);
@@ -112,4 +132,39 @@ function render() {
 window.onload = function () {
   init();
   render();
+  addEventListener();
 };
+
+window.onresize = function (){
+  camera.aspect = window.innerWidth/window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function addEventListener() {
+  document.addEventListener("mousedown", onMouseDown);
+}
+
+// let raycaster = new THREE.Raycaster();
+// let mouse = new THREE.Vector2()
+// function onMouseDown( event ) {
+
+//   // Get screen-space x/y
+//   mouse.x = ( event.clientX / renderer.domElement.clientWidth ) * 2 - 1;
+//   mouse.y = - ( event.clientY / renderer.domElement.clientHeight ) * 2 + 1;
+
+//   // Perform raycast
+//   raycaster.setFromCamera( mouse, camera );
+
+//   // See if the ray from the camera into the world hits our mesh
+//   const intersects = raycaster.intersectObject( floor );
+
+//   // Check if an intersection took place
+//   if ( intersects.length > 0 ) {
+//       const posX = intersects[0].point.x;
+//       const posZ = intersects[0].point.z;
+//       console.log(posX, posZ);
+//   }
+
+// }
